02c8affd54cbfd45ad54f3188ae90f3595def205
Julio Sena <juliosena.1411@gmail.com>
2026-03-23 16:53:35 -0300

    23_03

--- app/Filament/Resources/Clientes/ClienteResource.php
+++ app/Filament/Resources/Clientes/ClienteResource.php
@@ -34,7 +34,7 @@ class ClienteResource extends Resource
         ->schema([
             TextInput::make('nome')->required()->label('Nome Completo'),
             TextInput::make('email')->email()->label('E-mail'),
-            TextInput::make('telefone')->tel()->label('Telefone')->maks('(99) 99999-9999'),
+            TextInput::make('telefone')->tel()->label('Telefone')->mask('(99) 99999-9999'),
             TextInput::make('documento')->label('CPF ou CNPJ')->mask(RawJs::make(<<<'JS'
                 $input.length > 14 ? '99.999.999/9999-99' : '999.999.999-99'
             JS)),
